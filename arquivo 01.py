ce0f34739aae70f4a70087ac98e63ce79c7aad88
Bruno Dayron <brunodayron8@gmail.com>
2022-11-20 20:45:41 -0300

    resoluçãodeproblema

---
+++ arquivo 01.py
@@ -0,0 +1,4 @@
+print('Faço a soma de quaisquer dois números, quer ver?')
+x = input('Primeiro número: ')
+y = input('Outro número: ')
+print('A soma de', x,' com', y,' é:', x+y)
---
+++ primeiro scrit.py
@@ -0,0 +1,6 @@
+
+l1 = int(input("A largura do cômodo: "))
+l2 = int(input("O comprimento: "))
+vforro = int(input("Qual o valor do forro? "))
+vfinal = l1*l2*vforro
+print ("O valor do orçamento é: ",vfinal)
